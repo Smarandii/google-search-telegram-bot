bce125044b313d013de604163e67f1a1a037a949
Oleg <olegsmarandi@gmail.com>
2022-03-06 22:01:42 +0300

    init

---
+++ SearchBot/__init__.py
@@ -0,0 +1,4 @@
+from telebot import TeleBot
+
+bot = TeleBot(token="5038960617:AAHQw_9oXyv2HDquqb59IbMBbo13GJj2Ewc")
+SEARCH_API_KEY = "9c137aff6689199cfd9a001731fb1cab373688477dcd73803ea34ddeafb76da4"
---
+++ SearchBot/main.py
@@ -0,0 +1,60 @@
+from serpapi import GoogleSearch
+from __init__ import bot, SEARCH_API_KEY
+
+
+def txt_to_bold(txt: str) -> str:
+    __BOLD_SYMBOL_START = "*"
+    __BOLD_SYMBOL_END = "*"
+    txt = prepare_txt_4_md(txt)
+    return f"{__BOLD_SYMBOL_START}{txt}{__BOLD_SYMBOL_END}"
+
+
+def prepare_txt_4_md(txt: str) -> str:
+    __RESERVED_SMBLS = ['_', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']
+    txt = str(txt)
+    for smbl in __RESERVED_SMBLS:
+        txt = txt.replace(smbl, f"\\{smbl}")
+    return txt
+
+
+def get_string_results(message):
+    params = {
+        "engine": "duckduckgo",
+        "q": message.text,
+        "kl": "ru-ru",
+        "api_key": SEARCH_API_KEY
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    print(results)
+    organic_results = results['organic_results']
+    print(organic_results)
+    response = ""
+    counter = 0
+    for result in organic_results:
+        if counter >= 5:
+            break
+        title = f'{result["title"]}\n{result["link"]}'
+        response += f'{title}\n\n{result["snippet"]}\n\n\n'
+        counter += 1
+
+    print(response)
+    return response
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Поиск будет производиться по домену google.ru")
+
+
+@bot.message_handler(content_types=["text"])
+def send_results(message):
+    bot.send_message(message.chat.id, get_string_results(message), disable_web_page_preview=True)
+
+
+while True:
+    try:
+        bot.polling(non_stop=True)
+    except Exception as e:
+        bot.send_message(201973238, e.args)
